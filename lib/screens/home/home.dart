66d5dec84bef1231ec839310d1fde5d8c9ebbc95
Davi Arndt <davi-arndt@outlook.com>
2021-09-10 01:41:20 -0300

    Get List of Users

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,12 @@
+import 'dart:ffi';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:tcc_apiario/services/auth.dart';
+import 'package:tcc_apiario/services/database.dart';
+
+import 'usuario_list.dart';
 
 class Home extends StatelessWidget {
 
@@ -8,21 +15,26 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Scaffold(
-        backgroundColor: Colors.brown[50],
-        appBar: AppBar(
-          title: Text('Apiários'),
-          backgroundColor: Colors.brown[400],
-          elevation: 0.0,
-          actions: <Widget>[
-            TextButton.icon(
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-              onPressed: () async {
-                await _auth.signOut();
-              },
-            ),
-          ],
+      child: StreamProvider<QuerySnapshot?>.value(
+        initialData: null,
+        value: DatabaseService(uid: '').usuarios,
+        child: Scaffold(
+          backgroundColor: Colors.brown[50],
+          appBar: AppBar(
+            title: Text('Apiários'),
+            backgroundColor: Colors.brown[400],
+            elevation: 0.0,
+            actions: <Widget>[
+              TextButton.icon(
+                icon: Icon(Icons.person),
+                label: Text('logout'),
+                onPressed: () async {
+                  await _auth.signOut();
+                },
+              ),
+            ],
+          ),
+          body: UsuarioList(),
         ),
       ),
     );
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,11 +5,16 @@ class DatabaseService {
   final String uid;
   DatabaseService({ required this.uid });
   
-  final CollectionReference apiarioCollection = FirebaseFirestore .instance.collection('tcc-apiario');
+  final CollectionReference usuarioCollection = FirebaseFirestore .instance.collection('usuario');
 
   Future createDatabaseForNewUser(String name) async {
-    return await apiarioCollection.doc(uid).set({
+    return await usuarioCollection.doc(uid).set({
       'name': name
     });
   }
+
+  Stream<QuerySnapshot> get usuarios {
+    return usuarioCollection.snapshots();
+  }
+
 }
