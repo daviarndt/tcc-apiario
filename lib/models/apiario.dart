56fb4a9c77550a4d660b8d71c74cf055b86ef6a8
Davi Arndt <davi-arndt@outlook.com>
2021-09-10 01:41:31 -0300

    Apiario Model

---
+++ lib/models/apiario.dart
@@ -0,0 +1,17 @@
+class Apiario {
+
+  String nome;
+  String logradouro;
+  String latitude;
+  String longitude;
+  DateTime dataCriacao;
+
+  Apiario({
+    required this.nome,
+    required this.logradouro,
+    required this.latitude,
+    required this.longitude,
+    required this.dataCriacao
+  });
+
+}
