d677c96c652d88e1454219c9969add33b07c73a0
Davi Arndt <davi-arndt@outlook.com>
2021-09-14 21:45:30 -0300

    Stream Builder

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -7,6 +7,7 @@ import 'package:tcc_apiario/models/user_custom.dart';
 import 'package:tcc_apiario/screens/apiario/form_apiario.dart';
 import 'package:tcc_apiario/services/auth.dart';
 import 'package:tcc_apiario/services/database.dart';
+import 'package:tcc_apiario/shared/loading.dart';
 
 import 'apiario_list.dart';
 
@@ -17,38 +18,56 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<UserCustom>(context);
 
-    return Container(
-      child: StreamProvider<List<Apiario>?>.value(
-        initialData: null,
-        value: DatabaseService(uid: user.uid).apiarios,
-        child: Scaffold(
-          backgroundColor: Colors.brown[50],
-          appBar: AppBar(
-            title: Text('Apiários'),
-            backgroundColor: Colors.brown[400],
-            elevation: 0.0,
-            actions: <Widget>[
-              TextButton.icon(
-                icon: Icon(Icons.person),
-                label: Text('logout'),
-                onPressed: () async {
-                  await _auth.signOut();
-                },
-              ),
-            ],
-          ),
-          body: ApiarioList(),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => FormApiario(),
-                ),
-              );
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Apiários'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          TextButton.icon(
+            icon: Icon(Icons.person),
+            label: Text('logout'),
+            onPressed: () async {
+              await _auth.signOut();
             },
           ),
-        ),
+        ],
+      ),
+      body: StreamProvider<List<Apiario>?>.value(
+        initialData: null,
+        value: DatabaseService(uid: user.uid).apiarios,
+        child: StreamBuilder<List<Apiario>?>(
+            stream: DatabaseService(uid: user.uid).apiarios,
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error: ${snapshot.error}'),
+                );
+              }
+
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Loading();
+              }
+
+              if (snapshot.data!.length == 0) {
+                return Center(
+                  child: Text('Ainda não existem apiários cadastrados...'),
+                );
+              }
+
+              return ApiarioList();
+            }),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => FormApiario(),
+            ),
+          );
+        },
       ),
     );
   }
--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -17,18 +17,6 @@ class AuthService {
         .map(_userFromFirebaseUser);
   }
 
-  // Sign in anonymously
-  Future signInAnon() async {
-    try {
-      UserCredential result = await _auth.signInAnonymously();
-      User? user = result.user;
-      return _userFromFirebaseUser(user);
-    } catch(e) {
-      print(e.toString());
-      return null;
-    }
-  }
-
   // Sign in with email and password
   Future signInWithEmailAndPassword(String email, String password) async {
     try {
